4872bf78f2dd33835d41c3acb9dbf2e4e30bc507
Shashank <singh2020shashank@gmail.com>
2026-09-20 16:43:51 +0530

    fix: allow all hosts in ALLOWED_HOSTS

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,7 +12,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
 # Application definition
 DJANGO_APPS = [
